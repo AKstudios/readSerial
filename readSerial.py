56ab9c02fa3f7a6cbd33fd434295f183bb7660b6
AKstudios <akram@akstudios.com>
2016-07-11 12:34:16 -0500

    Enable syslog
    Replace default logging with syslog

--- readSerial.py
+++ readSerial.py
@@ -10,12 +10,21 @@ import requests
 import dictionary
 import os
 import time
-<<<<<<< HEAD
 import logging
+import logging.handlers
 
-# Configure logging
-logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-logging.debug("Starting script..")
+# configure syslog logging
+logger = logging.getLogger('MyLogger')
+logger.setLevel(logging.DEBUG)
+
+handler = logging.handlers.SysLogHandler(address = '/dev/log')
+
+logger.addHandler(handler)
+logger.debug("Starting script..")
+
+# Configure default logging
+#logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+#logging.debug("Starting script..")
 
 # Define sensor id, SQLdict and flag as global variables
 id = 0
@@ -23,31 +32,17 @@ flag = True
 SQLdict = {}
 
 # Select USB0 if radio is connected to first USB port on Pi
-logging.debug("Opening serial port")
+logger.debug("Opening serial port")
 try:
     serialport = serial.Serial("/dev/ttyUSB0", 115200) # make sure baud rate is the same
-    logging.info("Serial port open successfully")
+    logger.info("Serial port open successfully")
 except:
-    logging.warning("Serial port open FAILED")
-=======
-
-
-# Define sensor id and flag as global variables
-id = 0
-flag = True
-
-# Select USB0 if radio is connected to first USB port on Pi
-serialport = serial.Serial("/dev/ttyUSB0", 115200) # make sure baud rate is the same
->>>>>>> origin/master
+    logger.warning("Serial port open FAILED")
 
 # Set location of sqlite db & table name
 db = '/home/pi/serial_db.sqlite'
 tn = 'SERIAL_DB'
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
 # Create SQLLite Database, may already exist, handle exception
 def create_db():
     #tn = 'SERIAL_DB'    #table name
@@ -60,15 +55,15 @@ def create_db():
     nf4 = 'timestamp'
     ft4 = 'INTEGER'
 
-    logging.debug("Connecting to database")
+    logger.debug("Connecting to database")
     conn = sqlite3.connect(db) #connect to db
     c = conn.cursor()
 
-    logging.debug("Creating table/updating values")
+    logger.debug("Creating table/updating values")
     try:
         c.execute("CREATE TABLE IF NOT EXISTS {tn} ({nf1} {ft1}, {nf2} {ft2}, {nf3} {ft3}, {nf4} {ft4}, PRIMARY KEY({nf1},{nf2}))".format(tn=tn, nf1=nf1, ft1=ft1, nf2=nf2, ft2=ft2, nf3=nf3, ft3=ft3, nf4=nf4, ft4=ft4))
     except:
-        logging.error("SQL command error")
+        logger.error("SQL command error")
 
     conn.commit()   #commit changes to db
     conn.close()    #close connection
@@ -76,26 +71,22 @@ def create_db():
 
 # Read Serial port
 def read_serial():
-    logging.debug("Flushing serial I/O")
+    logger.debug("Flushing serial I/O")
     serialport.flushInput() #clear input serial buffer
     serialport.flushOutput() #clear output serial buffer
     while True: # keep reading serial port and write to file till the end of time
-        logging.debug("Waiting for serial data ...")
+        logger.debug("Waiting for serial data ...")
         #print("\nWaiting for serial data ...")
         data = serialport.readline() #expecting this format -> "i:1,t:25.44,h:40.23,l:34.00\n"
-        logging.info("Data received via serial")
+        logger.info("Data received via serial")
         #print data
         if data[0]=='i': # check if data is not empty and entire string is being sent (first value is always "i", which is node ID)
             final_data = parse(data) # parse data
             write_db(final_data)    # write data to SQLite db
             restAPI(final_data) # push data to HA via rest API
             checkFlag(final_data) # check if flag is False, then edit YAML files & restart HA
-<<<<<<< HEAD
         else:
-            logging.warning("Incomplete data packet received")
-=======
-            print("\nWaiting for serial data ...")
->>>>>>> origin/master
+            logger.warning("Incomplete data packet received")
 
 # Parse data string
 def parse(data):
@@ -103,14 +94,9 @@ def parse(data):
     for p in data.strip().split(","): #strip() removes trailing \n
         k,v = p.split(":")
         final_data[k] = v if v else 0.00
-<<<<<<< HEAD
-    logging.debug("Data parsed into dictionary")
+    logger.debug("Data parsed into dictionary")
     #print ("Final Data:")
     #print (final_data)
-=======
-    print ("Final Data:")
-    print (final_data)
->>>>>>> origin/master
     return final_data
 
 
@@ -124,47 +110,31 @@ def write_db(final_data): # one row per node, per sensor value, overwrite existi
 
     ts = int(time.time()) #get epoch time in seconds
     id = final_data.pop("i") #get sensor id
-<<<<<<< HEAD
     #id = 172
-=======
-    #id = 156
->>>>>>> origin/master
     #st = sensor type, sv = sensor value
-    logging.debug("Looking for data in SQLite DB")
+    logger.debug("Looking for data in SQLite DB")
     for k,v in final_data.iteritems():
         # check if data exists by checking node ID and sensor type
         a = "SELECT * FROM {tn} WHERE {nodeid} = '{id}' AND {stype}='{st}'".format(sv='sensor_value', stype='sensor_type', st=k, tn=tn, nodeid='node_id', id=id)
-<<<<<<< HEAD
         #print(a)
-        logging.debug(a)
+        logger.debug(a)
         try:
             c.execute(a)
         except:
-            logging.error("SQL command Error")
+            logger.error("SQL command Error")
         data = c.fetchone() #returns one matching row; fetchall() returns all matching rows
         if not data:
             flag = False
-            logging.debug("Node doesn't exist in DB")
+            logger.debug("Node doesn't exist in DB")
 
         st = "INSERT OR REPLACE INTO {tn} VALUES ({id},'{st}', '{sv}', {ts})".format(id=id, tn=tn, st=k, sv=v, ts=ts)
         #print st
-        logging.debug(st)
-        logging.debug("Writing data to SQLite DB")
+        logger.debug(st)
+        logger.debug("Writing data to SQLite DB")
         try:
             c.execute(st)
         except:
-            logging.error("SQL command Error")
-=======
-        print(a)
-        c.execute(a)
-        data = c.fetchone() #returns one matching row; fetchall() returns all matching rows
-        if not data:
-            flag = False
-
-        st = "INSERT OR REPLACE INTO {tn} VALUES ({id},'{st}', {sv}, {ts})".format(id=id, tn=tn, st=k, sv=v, ts=ts)
-        print st
-        c.execute(st)
->>>>>>> origin/master
+            logger.error("SQL command Error")
 
     conn.commit()   #commit changes to db
     conn.close() #close connection to SQL file after writing to it
@@ -172,7 +142,6 @@ def write_db(final_data): # one row per node, per sensor value, overwrite existi
 
 # Publish data to HA via restAPI
 def restAPI(final_data):
-<<<<<<< HEAD
     global id # use global id instead of local
     for k,v in final_data.iteritems():
         if k == '#': continue # skip group name
@@ -187,18 +156,6 @@ def restAPI(final_data):
                 'content-type': 'application/json'}
 
         data  = '{"state" : "%s", "attributes": {"friendly_name": "%s", "unit_of_measurement": "%s", "icon": "%s"}}' % (v, sn, su, si)
-=======
-    for k,v in final_data.iteritems():
-        #id = final_data.pop("i") #get sensor id
-        st = dictionary.sensorType[k]
-        su = dictionary.sensorUnit[k]
-
-        url = 'http://192.168.1.212:8123/api/states/sensor.%s_%s' % (st, id)
-        headers = {'x-ha-access': 'Abudabu1!',
-                'content-type': 'application/json'}
-
-        data  = '{"state" : "%s", "attributes": {"friendly_name": "%s", "unit_of_measurement": "%s"}}' % (v, st, su)
->>>>>>> origin/master
 
         req = requests.Request('POST', url, headers=headers, data=data)
         prepared = req.prepare()
@@ -206,17 +163,11 @@ def restAPI(final_data):
 
         #response = requests.post(url, headers=headers, data=data)
         #print(response.text)
-<<<<<<< HEAD
-        logging.debug("Sending data via REST API")
+        logger.debug("Sending data via REST API")
         #print("\nSending data via REST API\n")
         s = requests.Session()
         response = s.send(prepared)
-        logging.debug(response)
-=======
-        print("\nSending data via REST API\n")
-        s = requests.Session()
-        s.send(prepared)
->>>>>>> origin/master
+        logger.debug(response)
 
 
 # Print POST request in a pretty way
@@ -229,16 +180,15 @@ def pretty_print_POST(req):
     ))
 
 
-<<<<<<< HEAD
 # Rewrite sensors YAML file with new info
 def edit_sensors_YAML(final_data):
     global id # use global id instead of local
     filename = 'sensors.yaml'
-    logging.debug("Editing %s" % (filename,))
+    logger.debug("Editing %s" % (filename,))
     try:
         target = open('/home/pi/.homeassistant/%s' % (filename,), 'w') #open file in write mode
     except:
-        logging.error("Error opening %s" % (filename,))
+        logger.error("Error opening %s" % (filename,))
 
     target.truncate() # clear file
     target.write("#Sensors\n")
@@ -248,24 +198,11 @@ def edit_sensors_YAML(final_data):
         st = dictionary.sensorType[k]
         su = dictionary.sensorUnit[k]
         YAMLstring = "- platform: command_line\n  name: %s_%s\n  command: \"python /home/pi/readDb.py %s %s\"\n  unit_of_measurement: '%s'\n" % (st, id, k, id, su)
-=======
-# Rewrite custom sensors YAML file with new info
-def edit_customsensors_YAML(final_data):
-    target = open('/home/pi/.homeassistant/custom_sensors.yaml', 'w') #open file in write mode
-    target.truncate() # clear file
-    target.write("#Custom sensors\n")
-
-    for k,v in final_data.iteritems():
-        st = dictionary.sensorType[k]
-        su = dictionary.sensorUnit[k]
-        YAMLstring = "- platform: customsensor\n  name: %s %s\n  unit_of_measurement: '%s'\n" % (st, id, su)
->>>>>>> origin/master
         target.write(YAMLstring)
 
     target.write("\n")
     target.close() # important - close file
 
-<<<<<<< HEAD
     logging.debug("%s edited" % (filename,))
     #print("Adding new sensor to HA")
     #print("sensors.yaml edited")
@@ -275,11 +212,11 @@ def edit_customsensors_YAML(final_data):
 def edit_groups_YAML(final_data):
     global id # use global id instead of local
     filename = 'groups.yaml'
-    logging.debug("Editing %s" % (filename,))
+    logger.debug("Editing %s" % (filename,))
     try:
         target = open('/home/pi/.homeassistant/%s' % (filename,), 'w') #open file in write mode
     except:
-        logging.error("Error opening %s" % (filename,))
+        logger.error("Error opening %s" % (filename,))
 
     target.truncate() # clear file
     target.write("#Groups\n")
@@ -287,26 +224,11 @@ def edit_groups_YAML(final_data):
 
     for k,v in final_data.iteritems():
         if k == '#': continue # skip group name
-=======
-    print("Adding new sensor to HA")
-    print("custom_sensors.yaml edited")
-
-
-# Rewrite custom groups YAML file with new info
-def edit_customgroups_YAML(final_data):
-    target = open('/home/pi/.homeassistant/custom_groups.yaml', 'w') #open file in write mode
-    target.truncate() # clear file
-    target.write("#Custom groups\n")
-    target.write("Group {id}:\n  entities:\n".format(id=id))
-
-    for k,v in final_data.iteritems():
->>>>>>> origin/master
         st = dictionary.sensorType[k]
         su = dictionary.sensorUnit[k]
         YAMLstring = "    - sensor.%s_%s\n" % (st, id)
         target.write(YAMLstring)
 
-<<<<<<< HEAD
     gn = final_data.get('#', "Group {id}".format(id=id))    # get group name if exists
 
     target.write("    - script.export_{id}\n".format(id=id))
@@ -315,7 +237,7 @@ def edit_customgroups_YAML(final_data):
     target.write("\n")
     target.close() # important - close file
 
-    logging.debug("%s edited" % (filename,))
+    logger.debug("%s edited" % (filename,))
     #print("Adding new groups to HA")
     #print("groups.yaml edited")
 
@@ -324,35 +246,17 @@ def edit_customgroups_YAML(final_data):
 def edit_customize_YAML(final_data):
     global id # use global id instead of local
     filename = 'customize.yaml'
-    logging.debug("Editing %s" % (filename,))
+    logger.debug("Editing %s" % (filename,))
     try:
-        target = open('/home/pi/.homeassistant/%s' % (filename,), 'w') #open file in write mode
+        logger = open('/home/pi/.homeassistant/%s' % (filename,), 'w') #open file in write mode
     except:
-        logging.error("Error opening %s" % (filename,))
+        logger.error("Error opening %s" % (filename,))
 
     target.truncate() # clear file
     target.write("#Customize nodes\n")
 
     for k,v in final_data.iteritems():
         if k == '#': continue # skip group name
-=======
-    target.write("  name: Group {id}\n".format(id=id))
-
-    target.write("\n")
-    target.close() # important - close file
-
-    print("Adding new groups to HA")
-    print("custom_groups.yaml edited")
-
-
-# Rewrite custom customize YAML file with new info
-def edit_customcustomize_YAML(final_data):
-    target = open('/home/pi/.homeassistant/custom_customize.yaml', 'w') #open file in write mode
-    target.truncate() # clear file
-    target.write("#Customize custom nodes\n")
-
-    for k,v in final_data.iteritems():
->>>>>>> origin/master
         st = dictionary.sensorType[k]
         su = dictionary.sensorUnit[k]
         si = dictionary.sensorIcon[k]
@@ -360,12 +264,11 @@ def edit_customcustomize_YAML(final_data):
         YAMLstring = "sensor.%s_%s:\n  friendly_name: %s\n  icon: %s\n" % (st, id, sn, si)
         target.write(YAMLstring)
 
-<<<<<<< HEAD
     target.write("script.export_{id}:\n  icon: mdi:export\n".format(id=id))
     target.write("\n")
     target.close() # important - close file
 
-    logging.debug("%s edited" % (filename,))
+    logger.debug("%s edited" % (filename,))
     #print("Customizing new nodes")
     #print("customize.yaml edited")
 
@@ -374,11 +277,11 @@ def edit_customcustomize_YAML(final_data):
 def edit_shell_commands_YAML(final_data):
     global id # use global id instead of local
     filename = 'shell_commands.yaml'
-    logging.debug("Editing %s" % (filename,))
+    logger.debug("Editing %s" % (filename,))
     try:
         target = open('/home/pi/.homeassistant/%s' % (filename,), 'w') #open file in write mode
     except:
-        logging.error("Error opening %s" % (filename,))
+        logger.error("Error opening %s" % (filename,))
 
     target.truncate() # clear file
     target.write("#Shell commands\n")
@@ -389,7 +292,7 @@ def edit_shell_commands_YAML(final_data):
     target.write("\n")
     target.close() # important - close file
 
-    logging.debug("%s edited" % (filename,))
+    logger.debug("%s edited" % (filename,))
     #print("Adding export shell command")
     #print("shell_commands.yaml edited")
 
@@ -398,11 +301,11 @@ def edit_shell_commands_YAML(final_data):
 def edit_scripts_YAML(final_data):
     global id # use global id instead of local
     filename = 'scripts.yaml'
-    logging.debug("Editing %s" % (filename,))
+    logger.debug("Editing %s" % (filename,))
     try:
         target = open('/home/pi/.homeassistant/%s' % (filename,), 'w') #open file in write mode
     except:
-        logging.error("Error opening %s" % (filename,))
+        logger.error("Error opening %s" % (filename,))
 
     target.truncate() # clear file
     target.write("#Scripts\n")
@@ -413,7 +316,7 @@ def edit_scripts_YAML(final_data):
     target.write("\n")
     target.close() # important - close file
 
-    logging.debug("%s edited" % (filename,))
+    logger.debug("%s edited" % (filename,))
     #print("Adding export script")
     #print("scripts.yaml edited")
 
@@ -421,44 +324,36 @@ def edit_scripts_YAML(final_data):
 def create_SQL_dict():
     global SQLdict # use global variable
 
-    logging.debug("Connecting to database")
+    logger.debug("Connecting to database")
     conn = sqlite3.connect(db) #connect to db
     c = conn.cursor()
 
-    logging.debug("Creating dictionary of lists from database")
+    logger.debug("Creating dictionary of lists from database")
     try:
         c.execute("SELECT DISTINCT node_id FROM {tn}".format(tn=tn))
         ids = c.fetchall()
     except:
-        logging.error("SQL command error")
+        logger.error("SQL command error")
 
     for i in ids:
         SQLdict[i[0]] = []
         try:
             c.execute("SELECT DISTINCT sensor_type FROM {tn} WHERE {nodeid} = {id}".format(nodeid='node_id',tn=tn, id=i[0]))
         except:
-            logging.error("SQL command error")
+            logger.error("SQL command error")
         types = c.fetchall()
         SQLdict[i[0]] = [v[0] for v in types if v[0] != '#']
 
-    logging.debug("Dictionary of lists created")
+    logger.debug("Dictionary of lists created")
     conn.commit()   #commit changes to db
     conn.close()    #close connection
-=======
-    target.write("\n")
-    target.close() # important - close file
-
-    print("Adding new groups to HA")
-    print("custom_customize.yaml edited")
->>>>>>> origin/master
 
 
 # Check if flag is false, then edit YAML files & restart HA
 def checkFlag(final_data):
     global flag
     if not flag:
-<<<<<<< HEAD
-        logging.debug("New node detected")
+        logger.debug("New node detected")
         #print("New node detected")
         create_SQL_dict()
         edit_sensors_YAML(final_data)
@@ -467,7 +362,7 @@ def checkFlag(final_data):
         edit_scripts_YAML(final_data)
         edit_shell_commands_YAML(final_data)
         flag = True  # set flag as true
-        logging.info("Restarting Pi ...")
+        logger.info("Restarting Pi ...")
         #print("Restarting Pi ...")
         #os.system('sudo reboot')  # restart Pi
         #os.system('sudo systemctl restart home-assistant@pi') # very important - restart home assistant after editing config files
@@ -480,21 +375,6 @@ def checkFlag(final_data):
 
         os.system('sudo reboot')  # restart Pi
 
-=======
-        print("New node detected")
-        edit_customsensors_YAML(final_data)
-        edit_customgroups_YAML(final_data)
-        edit_customcustomize_YAML(final_data)
-        flag = True  # set flag as true
-        print("Restarting Home Assistant ...")
-        os.system('sudo systemctl restart home-assistant@pi') # very important - restart home assistant after editing config files
-        for i in range(10,0,-1):
-            time.sleep(1)
-            sys.stdout.write(str(i)+' ')
-            sys.stdout.flush()
-        #time.sleep(15) # delay for 15 seconds till HA is properly restarted
-
->>>>>>> origin/master
 
 #run
 create_db()
